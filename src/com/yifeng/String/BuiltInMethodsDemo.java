4b573ddd1bb48fcc0856a555b6a1db08aa3ef6a0
unknown <meixx115@d.umn.edu>
2018-06-10 11:19:22 -0500

    getChars and getBytes demo are added

---
+++ src/com/yifeng/String/BuiltInMethodsDemo.java
@@ -0,0 +1,55 @@
+package com.yifeng.String;
+
+import java.io.UnsupportedEncodingException;
+
+public class BuiltInMethodsDemo {
+
+	private static final String str1 = "abcdefghijklmnopqrstuvwxyz";
+	private static final String str2 = "America&China";
+	private static final String str3 = "1234567890";
+	
+	public static void main(String[] args) {
+		
+		// getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin)
+		char[] chars = new char[12];
+		str3.getChars(0, 10, chars, 0);
+		for(char c: chars)
+			System.out.print(c);
+		
+		// getBytes() will use default charset
+		byte[] bytes;
+		try {
+			bytes = str2.getBytes("ASCII");
+			for(byte b: bytes)
+				System.out.println(b);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		// String format 
+		
+		
+		// Simple regex match
+		/**
+		 * -?
+		 * -?\\d+
+		 * (-|\\+)
+		 * \s whitespace  \S not a whitespace
+		 * \d \D
+		 * \w [a-zA-Z0-9] \W
+		 * [^abc] negation
+		 * [a-z&&[^m-p]] intersection
+		 * XY X|Y
+		 * (X) a capturing group. You can refer to the ith
+		 * 
+		 */
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/yifeng/String/TestRegularExpression.java
@@ -0,0 +1,22 @@
+package com.yifeng.String;
+
+import java.util.regex.*;
+
+public class TestRegularExpression {
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Usage:\n regex + pattern");
+			System.exit(0);
+		}
+		for(String arg: args) {
+			
+			Pattern p = Pattern.compile(arg);
+			Matcher m = p.matcher(args[0]);
+			while(m.find()) {
+				System.out.println("Match "+ m.group() + " at positions" +
+						m.start() + "-" + (m.end() - 1));
+			}
+		}
+		
+	}
+}
